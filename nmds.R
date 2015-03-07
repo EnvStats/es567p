3f6bfaedb3aad2176ad34704a6abc5d47d2184ea
Bern67 <h2o.bern@gmail.com>
2015-03-06 22:48:43 -0800

    edits

--- nmds.R
+++ nmds.R
@@ -5,7 +5,7 @@
 ### Axis Scaling and Species Scores . 
 
 ### Adequacy is dependant on: 
-### Stress value < 10, check Shepard plot for outliers, Check distortion: select top 10% of values 
+### Stress value < .10, check Shepard plot for outliers, Check distortion: select top 10% of values 
 ### in Bray-Curtis matrix and compare to the NMDS ordination distance
 ### What to report:
 ### Distance measure used (BC), Algorithm/Software used, Number of runs, Number of dimensions (2) in the final solution
@@ -13,23 +13,25 @@
 
 ### ---- Data ----
 dta <- read.csv("apoch.csv")
-apo <- droplevels(subset(dta,eco4!="WCLV" & eco4!="WKF" & eco4!="PVB" & form!="R"))
-
-summary(apo)
+apoch <- droplevels(subset(dta,eco4!="WCLV" & eco4!="WKF" & eco4!="PVB" & form!="R"))
+apo <-apoch[-c(67),]
 ap <- apo[,c(2:8)]# multivariate response variable matrix (morphology)
 env <- apo[,c(9:14)] # Environemtal groups
+summary(apo)
+boxplot(ap)
+cor(ap)
+cor.matrix(ap)
 
-
-### -----NMDS Model-----
+### -----nMDS Model-----
 library(vegan)
 library(MASS)
-mod <- metaMDS(ap,k=3,trace=T,distance="bray",autotransform=F,trymax = 999) #MDS with 2 dimension
-names(mod)# what type of informatin is produced from NMDS.
-round(mod$species,2) # Variables contribution to MDS axis
+mod <- metaMDS(ap,k=3,trace=T,distance="bray",autotransform=F,trymax = 20) #nMDS with k=3 dimensions
+names(mod)# Type of informatin produced from nMDS.
+round(mod$species,2) # Variables contribution to nMDS axis
 
 par(mfrow=c(1,2)) #Stress plot and nMDS plot
 br<-vegdist(ap, method="bray") # calculate Bray-Curtis distance, or other dependant on data type
-stressplot(mod,br,p.col="lemonchiffon3",l.col="darkolivegreen",lwd=2, main="NMDS/Bray")
+stressplot(mod,br,p.col="lemonchiffon3",l.col="darkolivegreen",lwd=2, main="nMDS/Bray")
 legend("bottomright",c(paste("Stress =",round(mod$stress*100,2))))
 plot(mod,type="t", main=paste("NMDS/Bray"))
 
@@ -37,63 +39,66 @@ plot(mod,type="t", main=paste("NMDS/Bray"))
 cor(mod$points[,1],apo[,c(2:8)]) # Variable correlation with axis MDS1
 cor(mod$points[,2],apo[,c(2:8)]) # Variable correlation with axis MDS2
 
-par(mfrow=c(2,2)) # Correlation plots
-plot(mod$points[,1],apo[,3],xlab="MDS 1",ylab="chela", col="blue") # Plots original variable against MDS.
-legend("topright",c("r = -0.88"))
-plot(mod$points[,1],apo[,2],xlab="MDS 1",ylab="fem") # Only variables with r > .50
-legend("topright",c("r = -0.44"))
-plot(mod$points[,2],apo[,2],xlab="MDS 2",ylab="fem", col="red")
-legend("topright",c("r = -0.83"))
-plot(mod$points[,2],apo[,3],xlab="MDS 2",ylab="chela")
-legend("topright",c("r = 0.36"))
+par(mfrow=c(1,2)) # Correlation plots
+plot(mod$points[,1],apo[,3],xlab="nMDS 1",ylab="chela", col="blue") # Plots original variable against MDS.
+legend("topright",c("r = -0.91"))
+plot(mod$points[,2],apo[,2],xlab="nMDS 2",ylab="fem", col="red")
+legend("topright",c("r = 0.86"))
 
 ### Source cor.matrix, for NMDS 1 & 2 correlated with original variables
-MDS1 <- mod$points
+MDS <- mod$points
 apMDS <-cbind(MDS,ap)
 cor.matrix(apMDS)
 
 ####~~~~ Sydney's Plots ~~~~####
 par(mfrow=c(2,2))
-plot(mod$points[,1], mod$points[,2], pch=as.numeric(apo$sex),col=c("red","darkgreen")[apo$sex], xlab="NMDS 1", ylab="NMDS 2")
+plot(mod$points[,1], mod$points[,2], pch=as.numeric(apo$sex),col=c("red","darkgreen")[apo$sex], xlab="nMDS 1", ylab="nMDS 2")
 legend("topright",c("F","M"),pch=c(1,2),col=c("red","darkgreen"),inset=.02) 
-title(main="Chela gradient - Axis 1")
+title(main="Sex - Dim 1 vs Dim 2")
 abline(v=(seq(0,100,25)), col="lightgray", lty="dotted")
 abline(h=(seq(0,100,25)), col="lightgray", lty="dotted")
 text(mod, "species", col="grey", cex=0.8)
 
-plot(mod$points[,1], mod$points[,2], pch=as.numeric(apo$eco4),col=c(1,2,3,"grey",5)[apo$eco4], xlab="NMDS 1", ylab="NMDS 2") 
-legend("topright",c("CPL","NFRF","OCF","V","WH"),pch=c(1,2,3,4,5),col=c(1,2,3,4,5),inset=.02) 
-title(main="Eco4")
+plot(mod$points[,1], mod$points[,2], pch=as.numeric(apo$eco4),col=c(1,2,3,"grey",5)[apo$eco4], xlab="nMDS 1", ylab="nMDS 2") 
+legend("topright",c("CPL","NFRF","OCF","V","WH"),pch=c(1,2,3,4,5),col=c(1,2,3,"grey",5),inset=.02) 
+title(main="Eco4 - Dim 1 vs Dim 2")
 abline(v=(seq(0,100,25)), col="lightgray", lty="dotted")
 abline(h=(seq(0,100,25)), col="lightgray", lty="dotted")
 
-plot(mod$points[,1], mod$points[,2], pch=as.numeric(apo$form),col=c(2,"lightgrey",4)[apo$form], xlab="NMDS 1", ylab="NMDS 2") 
+plot(mod$points[,1], mod$points[,2], pch=as.numeric(apo$form),col=c(2,"lightgrey",4)[apo$form], xlab="nMDS 1", ylab="nMDS 2") 
 legend("topright",c("A","C","T"),pch=c(1,2,3),col=c(2,"lightgrey",4),inset=.02) 
-title(main="Femor gradient - Axis 2")
+title(main="Form - Dim 1 vs Dim 2")
 abline(v=(seq(0,100,25)), col="lightgray", lty="dotted")
 abline(h=(seq(0,100,25)), col="lightgray", lty="dotted")
 
 ### distance plot
 plot(apo$LAT, apo$LONG,type='n', xlab="Latitude", ylab="Longitude")
-symbols(apo$LAT, apo$LONG,circles=apo$fem, inches=0.2, add=T, lwd=2)  
+symbols(apo$LAT, apo$LONG,circles=apo$eco4, inches=0.2, add=T, lwd=2)  
 title(main="Sample site dispersion")
 
 ### ~~ Hypothesis Tests ~~ ####
 
 ### ANOSIM-test statistically whether there is a significant difference between 
 ### two or more groups of sampling units.
-ap.dist <-vegdist(ap)
+ap.dist <-vegdist(ap, "euclidean")
+# Mahalanobis distances are Euclidean distances of a matrix where columns are centred, 
+## have unit variance, and are uncorrelated. The index is not commonly used for community data, 
+## but it is sometimes used for environmental variables. The calculation is based on transforming 
+## data matrix and then using Euclidean distances following Mardia et al. (1979).
 attach(env)
+## If two groups of sampling units are really different in their species composition, 
+## then compositional dissimilarities between the groups ought to be greater than those within the groups.
+## The anosim statistic R is based on the difference of mean ranks between groups (r_B) and within groups (r_W):
 apoS.ano <-anosim(ap.dist,sex)
 apoF.ano <-anosim(ap.dist,form)
 apoE.ano <-anosim(ap.dist,eco4)
 summary(apoS.ano)
 summary(apoF.ano)
 summary(apoE.ano)
-par(mfrow=c(2,2))
-plot(apoS.ano, title(main="Gender"))
-plot(apoF.ano, title(main="Land form"))
-plot(apoE.ano, title(main="Eco4"))
+par(mfrow=c(2,2)) # For there to be a sig dif, the between disimilarity needs to be greater than the within groups
+plot(apoS.ano, title(main="Gender-Dim 1"), col = "lightgray") # Plot width is disimalarity within group
+plot(apoF.ano, title(main="Land form-Dim 2"), col = "blue")
+plot(apoE.ano, title(main="Eco4-Dim2"), col = "darkgreen")
 detach(env)
 
 ### Pairwise t-test and aov for differences between groups
